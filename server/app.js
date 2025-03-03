cf9c165b7a4d85172d132c7d5d2d2f6eb15a6c2e
rimo02 <ghoshrimo02@gmail.com>
2025-03-03 22:58:03 +0530

    fixed path

--- server/app.js
+++ server/app.js
@@ -60,12 +60,11 @@ setInterval(async () => {
 
 const path = require('path');
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client", "dist")));
+const buildPath = path.resolve(__dirname, "client", "dist");
+app.use(express.static(buildPath));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-    });
-}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+});
 
 module.exports = server
